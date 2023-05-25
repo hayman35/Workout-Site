d711771be164c940ffb1ff722df3a8247cc10b57
Hayman AbdelJabbar <hayman-35@hotmail.com>
2023-05-24 22:07:38 -0400

    Finished Search component

--- workout-site/src/components/SearchExercises.jsx
+++ workout-site/src/components/SearchExercises.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Box, Button, Stack, TextField, Typography} from "@mui/material"
 
 const SearchExercises = () => {
+  const [search, setSearch] = useState("")
+
+  const handleSearch = async () => {
+    if (search) {
+      // const exceriseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises`, {
+        
+      // })
+    }
+  }
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p='20px'>
+      <Typography fontWeight={700} color='#FF2625' sx={{lg: '44px', xs: '30px'}} mb='30px' textAlign='center'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField sx={{input: {fontWeight: '700', border: 'none', borderRadius: '4px'}
+        , width:{ lg: '800px', xs: '350px'}
+        , backgroundColor: '#fff'
+        , borderRadius: '40px'}} 
+        height='76px' 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value.toLowerCase())} 
+        placeholder='Search Exercises' 
+        type='text'/>
+        <Button className='search-btn'
+        sx={{bgcolor: '#FF2625',
+        textTransform: 'none', 
+        color: '#fff', 
+        width: {lg: '175px', xs: '80px'}, 
+        height: '56px', 
+        position: 'absolute', 
+        right: '0', 
+        fontSize: {lg: '20px', xs: '14px'}, 
+        borderRadius: '0px 4px'}}
+        onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
